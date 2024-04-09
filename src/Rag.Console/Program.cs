09348e5ef9d231036475b1bfab2b5e29ae25c6b8
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2024-04-09 11:09:47 +0200

    Use secrets to access gpt api.

---
+++ src/Rag.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddTransient<Chat>(); })
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        config.AddUserSecrets<Program>();
+    })
+    .Build();
+
+var my = host.Services.GetRequiredService<Chat>();
+await my.Start();
+
+public class Chat
+{
+    private readonly IConfiguration _cofig;
+
+    public Chat(IConfiguration cofig)
+    {
+        _cofig = cofig;
+    }
+
+    public async Task Start()
+    {
+        string gptApiKey = _cofig["gpt-api-key"];
+        Console.WriteLine($"API Key: {gptApiKey}");
+
+        Thread.Sleep(TimeSpan.MaxValue);
+    }
+}
